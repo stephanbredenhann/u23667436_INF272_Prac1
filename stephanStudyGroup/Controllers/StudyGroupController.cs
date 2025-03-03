52bd6f984a242e45b4f63fd782c78a024628176a
Stephan Bredenhann <Stephan Bredenhann@STEPHAN-NB>
2025-03-03 21:47:46 +0200

    Add project files.

---
+++ stephanStudyGroup/Controllers/StudyGroupController.cs
@@ -0,0 +1,29 @@
+﻿using stephanStudyGroup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace stephanStudyGroup.Controllers
+{
+    public class StudyGroupController : Controller
+    {
+        // GET: stephanStudyGroup
+        public ActionResult Index()
+        {
+            List<studyGroupMembers> members = new List<studyGroupMembers>
+            {
+                
+                new studyGroupMembers { StudentNumber = "u23667436", Name = "Stephan", Surname = "Bredenhann", Email = "u23667436@tuks.co.za" },
+                new studyGroupMembers { StudentNumber = "u22892549", Name = "Stephan", Surname = "De Vlliers", Email = "u22892549@tuks.co.za" },
+                new studyGroupMembers { StudentNumber = "u20782563", Name = "Marnus", Surname = "Vosser", Email = "u20782563@tuks.co.za" },
+                new studyGroupMembers { StudentNumber = "u21582698", Name = "Kenny", Surname = "Strydom", Email = "u21582698@tuks.co.za" },
+                new studyGroupMembers { StudentNumber = "u21335982", Name = "Albert", Surname = "Venter", Email = "u21335982@tuks.co.za" }
+            };
+
+            return View(members);
+            
+        }
+    }
+}
---
+++ stephanStudyGroup/Models/studyGroupMembers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace stephanStudyGroup.Models
+{
+	public class studyGroupMembers
+	{
+        [Key]
+        public string StudentNumber { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+    }
+}
